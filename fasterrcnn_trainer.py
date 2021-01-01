d75159df3000cd8d340b32388075e60da60e8002
dreamhigh0525 <dreamhigh0525@gmail.com>
2021-01-01 17:32:58 +0900

    modified test process

--- fasterrcnn_trainer.py
+++ fasterrcnn_trainer.py
@@ -14,13 +14,11 @@ class FasterRCNNTrainer:
     net: FasterRCNN
     optimizer: optim.SGD
     scheduler: optim.lr_scheduler.CosineAnnealingLR
-    best_acc: float
     start_epoch: int
     tensorboard_writer: SummaryWriter
 
     def __init__(self, conf: dict) -> None:
         super().__init__()
-        torch.backends.cudnn.benchmark = True
         self.net = self.__prepare_net(conf.get('classes', 3))
         param = [p for p in self.net.parameters() if p.requires_grad]
         print(len(param))
@@ -33,13 +31,13 @@ class FasterRCNNTrainer:
         #self.schedular = StepLR(self.optimizer, step_size=25, gamma=0.1)
         self.schedular = CosineAnnealingLR(self.optimizer, T_max=200)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        torch.backends.cudnn.benchmark = True
         self.net.to(self.device)
         self.best_acc = 0.0
         self.start_epoch = 0
         self.tensorboard_writer = SummaryWriter('./tensorboard_logs')
     
     def train(self, loaders: Dict[str, DataLoader], epochs: int, resume: bool=False) -> None:
-        self.best_acc = 0.0
         self.start_epoch = 0
         if resume:
             self.load_checkpoint()
@@ -56,74 +54,53 @@ class FasterRCNNTrainer:
                     self.net.eval()
 
                 running_loss = 0.0
-                correct = 0
-                total = 0
-
                 for batch_idx, (inputs, targets, image_ids) in enumerate(loaders[phase]):
                     inputs = list(input.to(device) for input in inputs)
                     targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
                     loss_dict = self.net(inputs, targets)
-                    #print(loss_dict)
+                    # {'loss_classifier': tensor(0.9050, grad_fn=<NllLossBackward>), 'loss_box_reg': tensor(0.1463, grad_fn=<DivBackward0>), 'loss_objectness': tensor(0.0120, grad_fn=<BinaryCrossEntropyWithLogitsBackward>), 'loss_rpn_box_reg': tensor(0.0026, grad_fn=<DivBackward0>)} 
                     loss = sum(loss for loss in loss_dict.values())
-                    #print(loss)
+                    print(loss, loss.item())
+                    # tensor(1.0659, grad_fn=<AddBackward0>)
                     if phase == 'train':
                         self.optimizer.zero_grad()
                         loss.backward()
                         self.optimizer.step()
                     
                     running_loss += loss.item()
-                    #_, predicted = outputs.max(1)
-                    #total += targets.size(0)
-                    #correct += predicted.eq(targets).sum().item()
-                    #accuracy = 100.*correct/total
-
-                    #progress_bar(batch_idx, len(loaders[phase]), 'Loss: %.3f'
-                    #     % (running_loss/(batch_idx+1),))
                     progress_bar(batch_idx, len(loaders[phase]), 'Loss: %.5f'
                          % (loss.item(),))
                 
-                #epoch_acc = 100.*correct / len(loaders[phase].dataset)
                 epoch_loss = running_loss / len(loaders[phase])
-
                 if phase == 'train':
                     self.schedular.step()
                     print('lr: %f' % (self.schedular.get_last_lr()[0]))
                     self.tensorboard_writer.add_scalar('training loss', epoch_loss, epoch)
                 #else:
                 #    self.tensorboard_writer.add_scalar('validation accuracy', epoch_acc, epoch)
-                '''
-                if phase == 'val' and epoch_acc > self.best_acc:
+                
+                if phase == 'val':
                     print('saving checkpoint...')
                     state = {
                         'net': self.net.state_dict(),
-                        'acc': epoch_acc,
                         'epoch': epoch
                     }
-                    torch.save(state, './checkpoint/model.pth')
-                    self.best_acc = epoch_acc
-                '''
+                    torch.save(state, './checkpoint/model.pth')                
 
     def test(self, loader: DataLoader) -> None:
         self.net.eval()
         device = self.device
         test_loss = 0
-        correct = 0
-        total = 0
+
         with torch.no_grad():
-            for batch_idx, (inputs, targets) in enumerate(loader):
-                inputs, targets = inputs.to(device), targets.to(device)
-                outputs = self.net(inputs)
-                loss = self.criterion(outputs, targets)
-
+            for batch_idx, (inputs, targets, image_ids) in enumerate(loader):
+                inputs = list(input.to(device) for input in inputs)
+                targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+                loss_dict = self.net(inputs, targets)
+                #print(loss_dict)
+                loss = sum(loss for loss in loss_dict.values())
                 test_loss += loss.item()
-                _, predicted = outputs.max(1)
-                total += targets.size(0)
-                correct += predicted.eq(targets).sum().item()
-
-                progress_bar(batch_idx, len(loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
-                
-                
+                progress_bar(batch_idx, len(loader), 'Loss: %.5f' % (loss.item(),))
     
     def save(self, path: str) -> None:
         torch.save(self.net.state_dict(), path)
@@ -134,9 +111,7 @@ class FasterRCNNTrainer:
     def load_checkpoint(self) -> None:
         checkpoint = torch.load('./checkpoint/model.pth')
         self.net.load_state_dict(checkpoint['net'])
-        self.best_acc = checkpoint['acc']
         self.start_epoch = checkpoint['epoch']
-        print(self.start_epoch, self.best_acc)
 
     def __prepare_net(self, num_classes: int) -> FasterRCNN:
         net = fasterrcnn_resnet50_fpn(pretrained=True)
